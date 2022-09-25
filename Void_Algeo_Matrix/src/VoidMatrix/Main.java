1bfe4547a1010abfe4611172d8f71ce32e00f85a
NerbSerg <16521431@mahasiswa.itb.ac.id>
2022-09-25 13:42:08 +0700

    New Project Java

---
+++ Void_Algeo_Matrix/src/VoidMatrix/Main.java
@@ -0,0 +1,9 @@
+package VoidMatrix;
+
+public class Main {
+    Matrix m = new Matrix();
+    // -Main 
+    // -MAtriks , tampil matriks 
+    // -determinan extend matriks
+
+}
---
+++ Void_Algeo_Matrix/src/VoidMatrix/Matrix.java
@@ -0,0 +1,5 @@
+package VoidMatrix;
+
+public class Matrix {
+    
+}
