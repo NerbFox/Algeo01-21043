7a909f654c2aacc8ac76f5144e04ad5b3042ed4b
Ghazi Akmal Fauzan <16521259@mahasiswa.itb.ac.id>
2022-09-29 15:34:41 +0700

    Fix Polynomial Interpolation
    Kode untuk total nilai x yang dicari dan ganti float menjadi double

--- Void_Algeo_Matrix/src/VoidMatrix/PolynomialInterpolation.java
+++ Void_Algeo_Matrix/src/VoidMatrix/PolynomialInterpolation.java
@@ -8,17 +8,17 @@ public class PolynomialInterpolation {
         Scanner myScanner = new Scanner(System.in);
 
         // Deklarasi Variabel
-        int n; // Total banyaknya titik
-        int count, count2; // Variabel loop, untuk menghitung count
+        int n, n2; // Total banyaknya titik
+        int count, count2, count3; // Variabel loop, untuk menghitung count
 
-        float[] arrayx = new float[100]; // Array limit 99
-        float[] arrayy = new float[100]; // Array limit 99
-        float x = 0;
-        float y = 0;
-        float numerator;
-        float denominator;
+        double[] arrayx = new double[100]; // Array limit 99
+        double[] arrayy = new double[100]; // Array limit 99
+        double x = 0;
+        double y = 0;
+        double numerator;
+        double denominator;
 
-        // Input User
+        // Input banyaknya titik dari user
         System.out.print("Masukkan total banyaknya titik: ");
         n = myScanner.nextInt(); // Menyimpan input di n
 
@@ -27,9 +27,9 @@ public class PolynomialInterpolation {
         for (count = 0; count < n; count++) // Memulai loop untuk nilai x
         {
             // Input nilai xi
-            System.out.print("Masukkan nilai x" + count + ": ");
+            System.out.print("x" + count + ": ");
             // Menyimpan nilai di Array, arrayx
-            arrayx[count] = myScanner.nextFloat();
+            arrayx[count] = myScanner.nextDouble();
         }
 
         // Input nilai y dari user
@@ -37,31 +37,37 @@ public class PolynomialInterpolation {
         for (count = 0; count < n; count++) // loop for Y
         {
             // Input nilai yi
-            System.out.print("Masukkan nilai y" + count + ": ");
+            System.out.print("y" + count + ": ");
             // Menyimpan nilai di Array, arrayy
-            arrayy[count] = myScanner.nextFloat();
+            arrayy[count] = myScanner.nextDouble();
         }
-        // Meminta user untuk memasukkan nilai x apa pun untuk mendapatkan nilai y yang
-        // sesuai
-        System.out.print("Masukkan titik x yang ingin dicari nilai y nya: ");
-        x = myScanner.nextFloat(); // Menyimpan nilai di x
 
-        // Loop pertama untuk menghitung polinomial
-        for (count = 0; count < n; count++) {
-            // Inisialisasi variabel
-            numerator = 1;
-            denominator = 1;
+        // Input User
+        System.out.print("Masukkan total banyaknya titik x yang ingin dicari: ");
+        n2 = myScanner.nextInt(); // Menyimpan input di n2
 
-            // Loop kedua untuk menghitung polinomial
-            for (count2 = 0; count2 < n; count2++) {
-                if (count2 != count) {
-                    numerator = numerator * (x - arrayx[count2]);
-                    denominator = denominator * (arrayx[count] - arrayx[count2]);
+        for (count3 = 0; count3 < n2; count3++){
+            // Meminta user untuk memasukkan nilai x apa pun untuk mendapatkan nilai y yang sesuai
+            System.out.print("Titik x yang ingin dicari nilai y nya: ");
+            x = myScanner.nextDouble(); // Menyimpan nilai di x
+
+            // Loop pertama untuk menghitung polinomial
+            for (count = 0; count < n; count++) {
+                // Inisialisasi variabel
+                numerator = 1;
+                denominator = 1;
+
+                // Loop kedua untuk menghitung polinomial
+                for (count2 = 0; count2 < n; count2++) {
+                    if (count2 != count) {
+                        numerator = numerator * (x - arrayx[count2]);
+                        denominator = denominator * (arrayx[count] - arrayx[count2]);
+                    }
                 }
+                y = y + (numerator / denominator) * arrayy[count];
             }
-            y = y + (numerator / denominator) * arrayy[count];
+            System.out.println("Saat x = " + x + "," + " y = " + y);
+            
         }
-        System.out.println("Saat x = " + x + "," + " y = " + y);
-        // }
     }
 }
